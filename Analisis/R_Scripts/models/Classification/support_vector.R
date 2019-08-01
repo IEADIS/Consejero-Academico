ccea53a742a204537c57701c28e2d675d6f8f5d2
Zekkenn <sebastian.mr47@gmail.com>
2019-07-31 19:00:14 -0500

    Train Changes, svm fails

--- Analisis/R_Scripts/models/Classification/support_vector.R
+++ Analisis/R_Scripts/models/Classification/support_vector.R
@@ -7,6 +7,10 @@
 train.svm <- function(data){
 
   # TRAIN MODEL
+  write( "===============DATA TRAIN===================", stdout() )
+  write( str(data), stdout() )
+  write( data$Grade3, stdout() )
+  write( "===============DATA TRAIN===================", stdout() )
   ctrl <- trainControl(method = "repeatedcv", classProbs=TRUE, number = 5, savePredictions = TRUE, allowParallel = TRUE)
   grid <- expand.grid(C = c(0.1,0.25,0.5,0.75,1))
   mod_fit <- train( Grade3 ~ Grade1 + Grade2,  data=data, method="svmLinear", family="binomial", 
